028b97aa4121fa891f2fd8b818d7046f6185ee6c
omg-weishao <844541721@qq.com>
2019-07-20 21:02:54 +0800

    change function to  calss, and render params  from childcompont

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 
-function App() {
-  return (
-//    <div className="App">
-//     hello World
-//    </div>
-	  <div className="container">
-	  	<div className="row">
-	  		<div className="col-xs-1 col-xs-offset-11">
-	  			<h1>Hello</h1>
-	  		</div>
-	  	</div>
-	  	
-	  	<div className="row">
-	  		<div className="col-xs-1 col-xs-offset-11">
-	  			<Header />
-	  		</div>
-	  	</div>
-	  </div>
-	  
-
-  );
+//function App() {
+//	const user={
+//	name:"xuebing",
+//	hobbies:["sports","readings"]
+//	}
+//  return (
+//	  <div className="container">
+//	  	<div className="row">
+//	  		<div className="col-xs-1 col-xs-offset-11">
+//	  			<h1>Hello</h1>
+//	  		</div>
+//	  	</div>
+//	  	
+//	  	<div className="row">
+//	  		<div className="col-xs-1 col-xs-offset-11">
+//	  			<Header name={"weishao"} age = {20} user ={user} />
+//	  		</div>
+//	  	</div>
+//	  </div>
+//	  
+//
+//  );
+//}
+class App extends Component {
+	render(){
+		const user={
+		name:"xuebing",
+		hobbies:["sports","readings"]
+		}
+	    return (
+		  <div className="container">
+		  	<div className="row">
+		  		<div className="col-xs-1 col-xs-offset-11">
+		  			<h1>Hello</h1>
+		  		</div>
+		  	</div>	
+		  	
+		  	<div className="row">
+		  		<div className="col-xs-1 col-xs-offset-11">
+		  			<Header name={"wehsao"} age ={20} user={user}/>
+		  		</div>
+		  	</div>	  	
+		  </div>
+		  );
+	}
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,37 @@
-import React from 'react';
-
-function Header() {
-  return (
-	  <div className="container">
-	  	<div className="row">
-	  		<div className="col-xs-1 col-xs-offset-11">
-	  			<h1>Home!</h1>
-	  		</div>
-	  	</div>
-	  </div>
-  );
+import React,{ Component } from 'react';
+import ProTypes from 'prop-types'
+//function Header() {
+//	var that =this;
+//	console.log(this.props); //函数形式不能使用this
+//  return (
+//	  <div className="container">
+//	  	<div className="row">
+//	  		<div className="col-xs-1 col-xs-offset-11">
+//	  			<h1>Home!</h1>
+//	  		</div>
+//	  	</div>
+//	  </div>
+//  );
+//}
+export default class Header extends Component{
+	
+	render(){
+		console.log(this.props);
+		return (
+		  <div className="container">
+		  	<div className="row">
+		  		<div className="col-xs-1 col-xs-offset-11">
+		  			<h1>my name is {this.props.name},age = {this.props.age}</h1>
+		  		</div>
+		  	</div>
+		  </div>
+	  	);
+	}
 }
 
-export default Header;
+//export default Header; // 可以放到 class前面
+	//父组件传入得参数检查，若类型不多，会报错
+	Header.proTypes={
+		name : ProTypes.string,
+		age : ProTypes.number
+	}
